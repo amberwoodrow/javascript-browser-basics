e6858f12a1398c76ddf31c3f95452161204fd44b
amberwoodrow <amberwoodrow@gmail.com>
2015-07-09 14:13:37 -0600

    added readme, javascript, and html

---
+++ README.md
@@ -0,0 +1,1 @@
+A link to the solved requirments [code pen](http://codepen.io/amberwoodrow/pen/OVZpYO)
---
+++ script.js
@@ -0,0 +1,3 @@
+var firstName = prompt("What's your first name?");
+var lastName = prompt("Enter your last name.");
+alert("Hello " + firstName + " " + lastName);
